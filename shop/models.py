02e4985f9edc49a825cb0cf6e01822aa66b639a0
Pouria <pouriagheitouri@gmail.com>
2026-07-16 01:10:35 +0330

    Phase 1 - Initialize GymShop project and database structure

---
+++ shop/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+    image = models.ImageField(upload_to="categories/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
